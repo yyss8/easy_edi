cad7574ac2064e74d45d91bd0b1fa19a03ce68a5
Shen Chen <schen89@live.com>
2020-08-30 01:29:40 -0400

    2.0.3改动: 列表管理支持排序改动

--- components/utilities/general.js
+++ components/utilities/general.js
@@ -9,7 +9,11 @@ export const formTableSorter = (tableKey, form, field, defaultSortOrder) => {
 
     const data = form.getFieldValue(tableKey);
 
-    if (!Boolean(data[aIndex][field]) || !Boolean(data[bIndex][field])) {
+    if (!Boolean(data[aIndex]) || !Boolean(data[aIndex][field])) {
+      return 1;
+    }
+
+    if (!Boolean(data[bIndex][field]) || !Boolean(data[bIndex][field])) {
       return -1;
     }
 
