de0c0dcbe01264aa1889e943ba3fc5f7e06640fd
Shen Chen <schen89@live.com>
2020-10-14 23:42:31 -0400

    添加上传提示

--- components/edi/EdiUploadTab/UploadDescriptionHint/UploadDescriptionHint.jsx
+++ components/edi/EdiUploadTab/UploadDescriptionHint/UploadDescriptionHint.jsx
@@ -16,6 +16,7 @@ const UploadDescriptionHint = (props) => {
       return (
         <React.Fragment>
           <h3>
+            通过<a href="/?type=754">ARN确认</a>文件可生成并提交标签生成文档<br />
             成功上传标签后请等待2-3秒然后到<a href='/?type=label'>已生成标签</a>查看生成结果.
           </h3>
           <h5>上传文件名请勿带有#符号, 否则将无法下载归档文件.</h5>
@@ -26,19 +27,42 @@ const UploadDescriptionHint = (props) => {
       return (
         <React.Fragment>
           <h3>
-            请至亚马逊Operational Analytics页面查看发送结果或查看<a href='/?type=notify'>网关通知</a>.
+            通过<a href="/?type=label-excel&fileType=archive">上传标签归档</a>文件可生成并提交发货通知<br />
+            上传或提交完成后至亚马逊Operational Analytics页面查看发送结果或查看<a href='/?type=notify'>网关通知</a>.
           </h3>
           <h5>1. 如果第一次发送错误, 没有生成ASN, 文件名加-force重新上传.</h5>
           <h5>2. 上传文件名请勿带有#符号, 否则将无法下载归档文件.</h5>
         </React.Fragment>
       );
 
+    case '855':
+      return (
+        <React.Fragment>
+          <h3>
+            通过<a href="/">订单信息</a>文件可生成并提交订单确认文档<br />
+            上传或提交完成后至亚马逊Operational Analytics页面查看发送结果或查看<a href='/?type=notify'>网关通知</a>.
+          </h3>
+          <h5>上传文件名请勿带有#符号, 否则将无法下载归档文件.</h5>
+        </React.Fragment>
+      );
+
     case '753':
+      return (
+        <React.Fragment>
+          <h3>
+            通过<a href="/">订单信息</a>文件可生成并提交ARN申请文档<br />
+            上传或提交完成后至亚马逊Operational Analytics页面查看发送结果或查看<a href='/?type=notify'>网关通知</a>.
+          </h3>
+          <h5>上传文件名请勿带有#符号, 否则将无法下载归档文件.</h5>
+        </React.Fragment>
+      );
+
     default:
       return (
         <React.Fragment>
           <h3>
-            请至亚马逊Operational Analytics页面查看发送结果或查看<a href='/?type=notify'>网关通知</a>.
+            通过<a href="/">订单信息</a>文件可生成并提交ARN申请文档<br />
+            上传或提交完成后至亚马逊Operational Analytics页面查看发送结果或查看<a href='/?type=notify'>网关通知</a>.
           </h3>
           <h5>上传文件名请勿带有#符号, 否则将无法下载归档文件.</h5>
         </React.Fragment>
